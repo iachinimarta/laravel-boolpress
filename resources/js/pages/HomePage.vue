4dbf05bacac4e370603f328406f85cdd4806962e
Marta Iachini <iachinimarta@gmail.com>
2022-11-05 20:04:08 +0100

    Modifiche: - inizio modifiche della homepage - problema con img di sfondo

--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -1,5 +1,43 @@
 <template>
-    <div class="container">Home Page</div>
+    <div>
+        <div class="card">
+            <div class="mi-bg-img p-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div>
+                                <h1>Heading lorem ipsum dolor sit amet, consetetur sadipscing.</h1>
+                                <p>
+                                    Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Dictum sit amet justo donec enim diam vulputate ut. Eu nisl
+                                    nunc mi ipsum faucibus.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-wrapper card-space">
+                <div class="card card-bg">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="p-5">
+                                <div class="col-12 col-lg-10 offset-lg-1">
+                                    <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor…</h5>
+                                    <p class="card-text">
+                                        Mauris facilisis non nisi ac consectetur. Donec venenatis tempor elementum. Sed sit amet dictum eros, gravida egestas enim. Etiam turpis metus, molestie ac enim id, imperdiet dapibus mi. Cras commodo neque risus, eget aliquet orci placerat vitae. Cras a nunc vitae risus consequat consectetur. Vestibulum et sem in ante consectetur tristique. Vivamus sit amet sapien diam. Vivamus congue, nibh sit amet laoreet tincidunt, tellus sapien tristique mauris, sit amet placerat nisl enim scelerisque risus. Curabitur auctor dui id facilisis malesuada. 
+                                    </p>
+                                    <a class="read-more" href="#">
+                                        <span class="text">Leggi di più</span>
+                                        <span class="visually-hidden">su Lorem ipsum dolor sit amet…</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +46,10 @@
     }
 </script>
 
-<style>
+<style scoped lang="scss">
+    .mi-bg-img {
+
+        background-image: url('https://unsplash.com/photos/5vHwwSndck8');
+    }
 
 </style>
